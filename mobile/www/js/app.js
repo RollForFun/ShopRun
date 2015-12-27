e2fa76a823bb93eee03927b2e114289a153d3417
Songqing Sun <sunsq1991@gmail.com>
2015-12-26 21:57:34 -0500

    auth

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -5,7 +5,54 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic',
+    'starter.controllers',
+    'starter.services', 
+    'ngCordova', 
+    'ngCookies', 
+    'ngResource'
+  ])
+
+.factory('authInterceptor', function($rootScope, $q, $cookies, $injector) {
+  var state;
+  return {
+    // Add authorization token to headers
+    request: function(config) {
+      config.headers = config.headers || {};
+      if ($cookies.get('token')) {
+        config.headers.Authorization = 'Bearer ' + $cookies.get('token');
+      }
+      return config;
+    },
+
+    // Intercept 401s and redirect you to login
+    responseError: function(response) {
+      if (response.status === 401) {
+        (state || (state = $injector.get('$state'))).go('login');
+        // remove any stale tokens
+        $cookies.remove('token');
+        return $q.reject(response);
+      }
+      else {
+        return $q.reject(response);
+      }
+    }
+  };
+})
+
+.run(function($rootScope, $state, Auth) {
+  // Redirect to login if route requires auth and the user is not logged in
+  $rootScope.$on('$stateChangeStart', function(event, next) {
+    if (next.authenticate) {
+      Auth.isLoggedIn(function(loggedIn) {
+        if (!loggedIn) {
+          event.preventDefault();
+          $state.go('login');
+        }
+      });
+    }
+  });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
